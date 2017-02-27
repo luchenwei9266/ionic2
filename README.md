960976068ab886da19e655374063868d01dd4a79
luchenwei9266 <a413423382@163.com>
2017-02-27 15:58:57 +0800

    学习用

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# Ionic2
-
 # Ionic2学习自用
 
 `1.npm install`
 
-`2.ionic serve `
+`2.ionic serve`
+
+[官方文档](http://ionicframework.com/docs/)
+
+[中文文档(非官方)](https://yanxiaodi.gitbooks.io/ionic2-guide/content/introduction/)
+
+[中文API文档(非官方)](http://rainey.space/2016/04/06/Ionic2_Chinese_Document/)
--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from 'ionic-angular';
 import { ActionSheet } from '../ActionSheets/actionSheets';
 import { ActionSheetController } from 'ionic-angular';
 
@@ -8,8 +9,10 @@ import { ActionSheetController } from 'ionic-angular';
 })
 
 export class HelloIonicPage {
+
   constructor(
-    public actionSheetCtrl: ActionSheetController
+    public actionSheetCtrl: ActionSheetController,
+    public alertCtrl: AlertController
   ){ }
   
   // 打开底部菜单
@@ -41,4 +44,121 @@ export class HelloIonicPage {
     });
     actionSheet.present();
   }
+
+  openAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Hello My Friend',
+      subTitle: 'My Fiend,this is subTitle!',
+      buttons: ['OK','Haha','Cancel']
+    });
+    alert.present();
+  }
+
+  doPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'Login',
+      message: "Enter a name for this new album you're so keen on adding",
+      inputs: [
+        {
+          name:'title',
+          placeholder: 'Title'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Save',
+          handler: data => {
+              console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Cancel',
+          handler: data => {
+              console.log('Saved clicked');
+          }
+        }
+      ],
+    });
+    prompt.present();
+  }
+
+  doConfirm() {
+    let confirm = this.alertCtrl.create({
+      title: 'Use this lightsaber?',
+      message: 'Do you agree to use this lightsaber to do good across the intergalactic',
+      buttons: [
+        {
+          text: 'Disagree',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Agree',
+          handler: () => {
+              console.log('Agree clicked');
+          }
+         }
+      ]
+    });
+    confirm.present();
+  }
+
+  testRadioOpen:boolean;
+  testRadioResult;
+  doRadio() {
+    // console.log(this.testRadioOpen);
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Lightsaber color');
+    alert.addInput({
+        type: 'radio',
+        label: 'Blue',
+        value: 'blue',
+        checked: true
+    });
+    alert.addButton('Cancel');
+    alert.addButton({
+        text: 'OK',
+        handler: data => {
+            this.testRadioOpen = false;
+            this.testRadioResult = data;
+        }
+    });
+    alert.present();
+  }
+
+
+  testCheckboxOpen: boolean;
+  testCheckboxResult;
+  doCheckbox() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Which planets have you visited?');
+    alert.addInput({
+        type: 'checkbox',
+        label: 'Alderaan',
+        value: 'value1',
+        checked: true
+    });
+    alert.addInput({
+        type: 'checkbox',
+        label: 'Bespin',
+        value: 'value2'
+    });
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Tatooine',
+      value: 'value6'
+    });
+    alert.addButton('Cancel');
+    alert.addButton({
+        text: 'Okay',
+        handler: data => {
+            console.log('Checkbox data:', data);
+            this.testCheckboxOpen = false;
+            this.testCheckboxResult = data;
+        }
+    });
+    alert.present();
+  }
+
 }
